377f13a66a3c668cd3ab55a40e1e57b202a94323
David <debukali@gmail.com>
2025-08-07 05:09:11 +0300

    Migration to add description and imageurl fields to product and station models

---
+++ prisma/migrations/20250807020828_add_description_and_imageurl_fields_to_products_and_station_models/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "Station" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "description" TEXT,
+ADD COLUMN     "imageUrl" TEXT;
